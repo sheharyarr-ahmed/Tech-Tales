e67137fc2955e1d951709ea836d5500e7716b37a
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2026-01-26 13:03:18 +0500

    got to know about the context state managememt even further and its two parts which are context provider and custom hooks

--- src/PostContext.js
+++ src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 const PostContext = createContext();
@@ -48,5 +48,8 @@ function PostProvider({ children }) {
     </PostContext.Provider>
   );
 }
-
-export { PostProvider, PostContext };
+function usePosts() {
+  const context = useContext(PostContext);
+  return context;
+}
+export { PostProvider, usePosts };
